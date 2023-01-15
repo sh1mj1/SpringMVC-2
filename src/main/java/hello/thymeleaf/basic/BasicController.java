b9245e034d9642e664c1f4aeaf6b899919a99690
심지훈 <simjihun@simjihuns-MacBook-Air.local>
2023-01-15 17:02:07 +0900

    11. 반복

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -91,10 +91,23 @@ public class BasicController {
     }
 
     @GetMapping("/attribute")
-    public String attribute(){
+    public String attribute() {
         return "basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
 
     @Data
     static class User {
